90bab6092ede47e17105a2785ca183a4937b9972
Lucas <lucasfreiresabino@gmail.com>
2023-01-15 18:19:49 -0700

    in progress

---
+++ index.swift
@@ -0,0 +1,12 @@
+func solution(string: String ){
+    var resultString = ""
+    let arrayLetter = string.split(separator: "")
+    
+    for letter in arrayLetter{
+        if letter.isCased{
+            
+        }
+    }
+}
+
+solution(string: "camelCasing")
